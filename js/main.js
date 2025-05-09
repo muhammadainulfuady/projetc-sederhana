e2f5283b34b9bc237b7713860068e355abd68990
= <=>
2025-05-09 14:12:33 +0700

    new feature in folder/subfolder

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-function login() {
+function login(event) {
+  event.preventDefault();
+
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   if (username === "luffy" && password === "skyblue") {
     window.location.href = "./pages/main.html";
   } else {
-    alert("404Notfound");
+    alert("404 Not Found");
   }
 }
